a3c6bed0a82416952a31fd73e822e50642e4e259
vbulaniy <jetbul@gmail.com>
2018-10-28 15:07:26 +0300

    Without subscribtion

--- src/Bot.java
+++ src/Bot.java
@@ -4,52 +4,94 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Bot extends TelegramLongPollingBot {
 
 
     public static void main(String[] args) {
-
-
+     /*
+        System.getProperties().put("proxySet", "true");
+        System.getProperties().put("socksProxyHost", "145.239.93.148");
+        System.getProperties().put("socksProxyPort", "1080");
+*/
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-
-    //    System.getProperties().put( "proxySet", "true" );
-      //  System.getProperties().put("socksProxyHost", " 206.189.101.248");
-        //System.getProperties().put("socksProxyPort", "1080");
-
-
-
 
 
         try {
             telegramBotsApi.registerBot(new Bot());
-        }
-        catch (TelegramApiException e) {
+        } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
 
 
-
     @Override
     public void onUpdateReceived(Update update) {
-
+        Model model = new Model();
         Message message = update.getMessage();
-        if (message != null && message.hasText()){
-            if(message.getText().equals("/help")){
-                sendMsg(message, "Что я могу для Вас сделать?");
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/help":
+                    sendMsg(message, "Что я могу для Вас сделать?");
+                    break;
+                case "/settings":
+                    sendMsg(message, "Что настроить?");
+                    break;
+                case "/start":
+                    sendMsg(message, "Привет!!");
+                    break;
+                case "Ты кто?":
+                    sendMsg(message, "Вова!!");
+                    break;
+                case "Что ты делаешь?":
+                    sendMsg(message, "ЛЮБЛЮ ОКСАНУ!!");
+                    break;
+                default:
+                    try {
+                        sendMsg(message, Weather.getWeather(message.getText(), model));
+                    } catch (IOException e) {
+                        sendMsg(message, "Город не найден");
+                    }
             }
-            else
-                sendMsg(message, "Я Вас не понимаю");
 
+        }//else   sendMsg(message, "Я Вас не понимаю");
 
-        }
+    }
+
+    //Клавиатура
+    public void setButtons(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        //Связать клавиатуру и сообщение
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        //Подгонка клавиатуры по размеру
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        //Скрывать клавиатуру после ввода
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+//Первый ряд кнопок
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        keyboardFirstRow.add(new KeyboardButton("Ты кто?"));
+        keyboardFirstRow.add(new KeyboardButton("Что ты делаешь?"));
+// Добавить кнопки в список клавиатуры
+        keyboardRowList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
 
 
     }
+
 
     private void sendMsg(Message message, String text) {
         SendMessage sendMessage = new SendMessage();
@@ -57,10 +99,11 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(text);
-       try {
-           execute(sendMessage);
-        }
-        catch (TelegramApiException e){
+        try {
+            //Добавить клавиатуру к отправке сообщений
+            setButtons(sendMessage);
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
             e.printStackTrace();
         }
 
---
+++ src/Weather.java
@@ -0,0 +1,47 @@
+import netscape.javascript.JSObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Weather {
+
+    public static String getWeather(String message, Model model) throws IOException {
+
+        URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + message + "&units=metric&appid=886705b4c1182eb1c69f28eb8c520e20");
+
+        Scanner in = new Scanner((InputStream) url.getContent());
+        String result = " ";
+        while (in.hasNext()) {
+            result += in.nextLine();
+        }
+
+        JSONObject object = new JSONObject(result);
+        model.setName(object.getString("name"));
+
+        JSONObject main = object.getJSONObject("main");
+        model.setTemp(main.getDouble("temp"));
+        model.setHumidity(main.getDouble("humidity"));
+        model.setPressure(main.getDouble("pressure"));
+
+        JSONArray getArray = object.getJSONArray("weather");
+        for (int i = 0; i < getArray.length(); i++) {
+            JSONObject obj = getArray.getJSONObject(i);
+            model.setIcon((String) obj.get("icon"));
+            model.setMain((String) obj.get("main"));
+        }
+
+        return "Город: " + model.getName() + "\n" +
+                "Температура: " + model.getTemp() + " C" + "\n" +
+                "Влажность: " + model.getHumidity() + " %" + "\n" +
+                "Давление: " + model.getPressure() + " мм рт.ст." + "\n" +
+                "Описание погоды: " + model.getMain() + "\n" +
+                "https://openweathermap.org/img/w/" + model.getIcon() + ".png";
+
+
+    }
+}
